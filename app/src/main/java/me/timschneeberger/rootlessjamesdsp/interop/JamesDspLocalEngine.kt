c70c911d8fb18f472f3a38be1fb3472834c5eb15
sleepsinfields <cavemanbiome@gmail.com>
2025-11-24 14:13:32 -0900

    Update JamesDspLocalEngine.kt

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspLocalEngine.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspLocalEngine.kt
@@ -170,14 +170,15 @@ override fun setStereoArbEqFlagsInternal(
     master: Boolean,
     left: Boolean,
     right: Boolean
-) {
+): Boolean {
     JamesDspWrapper.setArbEqStereoFlags(
         handle,
-        global,
-        master,
-        left,
-        right
+        globalEnable = global,
+        masterEnable = master,
+        leftEnable = left,
+        rightEnable = right
     )
+    return true
 }
 
     override fun setLiveprogInternal(enable: Boolean, name: String, script: String): Boolean {
