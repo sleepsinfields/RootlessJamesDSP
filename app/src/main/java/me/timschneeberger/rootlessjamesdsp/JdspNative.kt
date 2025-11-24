8cd91a4a430f2a0fda9ad8b4431a68b9e31752e4
sleepsinfields <cavemanbiome@gmail.com>
2025-11-24 10:20:51 -0900

    Update JdspNative.kt

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/JdspNative.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/JdspNative.kt
@@ -1,17 +1,18 @@
 package me.timschneeberger.rootlessjamesdsp
 
 object JdspNative {
-
-    // If necessary:
-    // init {
-    //     System.loadLibrary("jamesdsp") // adjust name if needed
-    // }
-
     @JvmStatic
     external fun setStereoArbEqFlags(
         global: Boolean,
         master: Boolean,
         left: Boolean,
         right: Boolean
+    )
+
+    @JvmStatic
+    external fun setStereoArbEqCurves(
+        master: String,
+        left: String,
+        right: String
     )
 }
