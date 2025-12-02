690c1cc1c3cea6760cb3ee7793f5c6b386562995
sleepsinfields <cavemanbiome@gmail.com>
2025-12-02 13:55:41 -0900

    Update JamesDspRemoteEngine.kt

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspRemoteEngine.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspRemoteEngine.kt
@@ -175,13 +175,14 @@ class JamesDspRemoteEngine(
     }
 
 
-    override fun setVacuumTube(enable: Boolean, level: Double): Boolean {
+   override fun setVacuumTube(enable: Boolean, level: Double): Boolean {
     var ret = true
     if (enable) {
-        // scaled is still a Float because AudioEffect uses shorts under the hood
-        val scaled = (level * 1000.0).toFloat()
-        ret = effect.setParameter(150, scaled.roundToInt().toShort()) == AudioEffect.SUCCESS
-        Log.e("TubeDebug", "RemoteEngine → DSP: level=$level scaled=$scaled")
+        ret = effect.setParameter(
+            150,
+            (level * 1000.0).roundToInt().toShort()
+        ) == AudioEffect.SUCCESS 
+Log.e("TubeDebug", "RemoteEngine → DSP: level=$level scaled=$scaled")
     }
     return ret && (effect.setParameter(1206, enable.toShort()) == AudioEffect.SUCCESS)
 }
