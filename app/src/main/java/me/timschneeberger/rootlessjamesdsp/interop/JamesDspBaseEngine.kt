d7133d774a3f14ffe61f1f4b11a0f25e96c19e0d
sleepsinfields <cavemanbiome@gmail.com>
2025-12-02 16:15:14 -0900

    Update JamesDspBaseEngine.kt

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspBaseEngine.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspBaseEngine.kt
@@ -128,7 +128,11 @@ abstract class JamesDspBaseEngine(
 val tubeEnabled = cache.get(R.string.key_tube_enable, false)
 val tubeDrive   = cache.get(R.string.key_tube_drive, 2.0)  // Double
 
-Log.e("TubeDebug", "syncWithPreferences: enabled=$tubeEnabled drive=$tubeDrive")
+Log.e(
+    "TubeDebug",
+    "syncWithPreferences(engine=${System.identityHashCode(this)}): " +
+        "enabled=$tubeEnabled drive=$tubeDrive"
+)
 //
 
             cache.select(Constants.PREF_DDC)
