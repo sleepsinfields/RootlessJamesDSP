b0c6c05fef01d5e860ea917a1d0e76bebefbc80c
sleepsinfields <cavemanbiome@gmail.com>
2025-12-03 17:12:50 -0900

    Update JamesDspLocalEngine.kt

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspLocalEngine.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/interop/JamesDspLocalEngine.kt
@@ -149,17 +149,15 @@ class JamesDspLocalEngine(
     }
 
     // ---- Graphic EQ (mono) ----
-override fun setGraphicEq(enable: Boolean, bands: String): Boolean {
-    // Debug so we can see what's coming in
+override fun setGraphicEqInternal(enable: Boolean, bands: String): Boolean {
     Timber.e(
         "GeqDebug",
-        "LocalEngine.setGraphicEq enable=$enable bands=${bands.take(80)}"
+        "LocalEngine.setGraphicEqInternal enable=$enable bands=${bands.take(80)}"
     )
     return JamesDspWrapper.setGraphicEq(handle, enable, bands)
 }
 
-// ---- Stereo Graphic EQ curves (M/L/R) ----
-// master / left / right are the GraphicEQ: ... strings (nullable)
+// 
 fun updateStereoGraphicEq(
     master: String?,
     left: String?,
@@ -228,18 +226,7 @@ override fun setStereoArbEqFlagsInternal(
     )
 }
 //
-fun updateStereoGraphicEq(
-    master: String?,
-    left: String?,
-    right: String?
-): Boolean {
-    Timber.e(
-        "GeqDebug",
-        "LocalEngine.updateStereoGraphicEq: master=${master?.take(64)} " +
-        "left=${left?.take(64)} right=${right?.take(64)}"
-    )
-    return JamesDspWrapper.updateStereoGraphicEq(handle, master, left, right)
-}
+
 //
 
 
