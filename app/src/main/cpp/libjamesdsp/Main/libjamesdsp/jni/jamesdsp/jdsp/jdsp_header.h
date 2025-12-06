80a7eff9b5983a1657523c3a292e77ec1149e048
sleepsinfields <cavemanbiome@gmail.com>
2025-12-06 12:33:40 -0900

    Update jdsp_header.h

--- app/src/main/cpp/libjamesdsp/Main/libjamesdsp/jni/jamesdsp/jdsp/jdsp_header.h
+++ app/src/main/cpp/libjamesdsp/Main/libjamesdsp/jni/jamesdsp/jdsp/jdsp_header.h
@@ -652,13 +652,16 @@ extern void VTProcess(VacuumTube *tb, float *x1, float *x2,
                       float *out1, float *out2, size_t n);
 extern void VacuumTubeEnable(JamesDSPLib *jdsp);
 extern void VacuumTubeDisable(JamesDSPLib *jdsp);
+extern void VacuumTubeProcess(JamesDSPLib *jdsp, size_t n);
+
 extern void VacuumTubeSetGain(JamesDSPLib *jdsp, double dbGain);
 extern void VacuumTubeSetShape(JamesDSPLib *jdsp, double mix);
-extern void VacuumTubeProcess(JamesDSPLib *jdsp, size_t n);
 extern void VacuumTubeSetHarmonics(JamesDSPLib *jdsp, double even, double odd);
-extern void VacuumTubeSetTriodeParams(JamesDSPLib *jdsp,
-                                      double drive, double bias, double scale);
 extern void VacuumTubeSetHarmScale(JamesDSPLib *jdsp, double scale);
+extern void VacuumTubeSetTriodeParams(JamesDSPLib *jdsp, double drive, double bias, double scale);
+
+// NEW:
+extern void VacuumTubeSetCoreTriode(JamesDSPLib *jdsp, int enabled);
 
 // Live programmable effect
 extern const char* checkErrorCode(int errCode);
