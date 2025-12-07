fd15f171604eb20118a5cffcf7f95b099e83fe6c
sleepsinfields <cavemanbiome@gmail.com>
2025-12-07 09:03:31 -0900

    Update jdsp_header.h

--- app/src/main/cpp/libjamesdsp/Main/libjamesdsp/jni/jamesdsp/jdsp/jdsp_header.h
+++ app/src/main/cpp/libjamesdsp/Main/libjamesdsp/jni/jamesdsp/jdsp/jdsp_header.h
@@ -140,7 +140,7 @@ typedef struct str_dynfreqdomain
 
 typedef struct
 {
-    int needOversampling;
+    int needOversample;
     samplerateTool smp[2];
     SixBandsCrossover subband[2];
     float pregain, postgain;
